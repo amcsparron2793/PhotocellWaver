63e3a9b7acc2e90649284e4c83f39b3cae5a3202
amcsparron2793 <69968398+amcsparron2793@users.noreply.github.com>
2024-01-13 13:38:33 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+"""
+PhotocellWaver
+Reads a photocell and turns a servo to keep the lights on.
+"""
+
+from machine import ADC, Pin, PWM
+from picozero import Button
+from servo import Servo
+from time import sleep, sleep_ms
+
+
+def Wave(servo_pin: int = 0, led: Pin = Pin(25, Pin.OUT), **kwargs):
+    print("this is a wave")
+    servo = Servo(pin_id=servo_pin)
+    delay_ms = 15  # Amount of milliseconds to wait between servo movements
+
+    if kwargs:
+        if 'delay_ms' in kwargs:
+            if type(kwargs['delay_ms']) != int:
+                raise TypeError('delay_ms must be an integer.')
+            else:
+                delay_ms = kwargs['delay_ms']
+
+    for position in range(0, 180):  # Step the position forward from 0deg to 180deg
+        # print(position)  # Show the current position in the Shell/Plotter
+        servo.write(position)  # Set the Servo to the current position
+        sleep_ms(delay_ms)  # Wait for the servo to make the movement
+
+    for position in reversed(range(0, 180)):  # Step the position reverse from 180deg to 0deg
+        # print(position)  # Show the current position in the Shell/Plotter
+        servo.write(position)  # Set the Servo to the current position
+        sleep_ms(delay_ms)  # Wait for the servo to make the movement
+
+    onboardLED_Blink(led)
+
+
+def onboardLED_Blink(led: Pin = Pin(25, Pin.OUT), sleep_length: float = 0.5):
+    led.on()
+    print("LED on")
+    sleep(sleep_length)
+    led.off()
+    print("LED off")
+    sleep(sleep_length)
+
+
+def ReadPhotoCell(**kwargs):
+    onboard_led_pin = Pin(25, Pin.OUT)
+    photocell_pin = 27
+    dark_threshold = 1000
+    manual_button_pin = 18
+
+    if kwargs:
+        if 'photocell_pin' in kwargs and type(kwargs['photocell_pin']) == int:
+            photocell_pin = kwargs['photocell_pin']
+        if 'dark_threshold' in kwargs and type(kwargs['dark_threshold']) == int:
+            dark_threshold = kwargs['dark_threshold']
+        if 'manual_button_pin' in kwargs and type(kwargs['manual_button_pin']) == int:
+            manual_button_pin = kwargs['manual_button_pin']
+
+    ldr = ADC(photocell_pin)
+    wave_button = Button(manual_button_pin)
+    while True:
+        has_waved = False
+        if wave_button.is_active:
+            # this can only work when the servo and led pins are the defaults
+            wave_button.when_activated = Wave
+            has_waved = True
+
+        reading = ldr.read_u16()
+        print(f"Photocell reading: {reading}")
+        if reading >= dark_threshold and not has_waved:
+            Wave(led=onboard_led_pin)
+            has_waved = True
+        if has_waved:
+            print("has waved")
+        sleep(1)
+
+
+if __name__ == '__main__':
+    ReadPhotoCell()
