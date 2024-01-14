0aab4a6a691ad46c5644ff1da8f604e44e5e40a9
amcsparron2793 <69968398+amcsparron2793@users.noreply.github.com>
2024-01-13 20:03:27 -0500

    start/stop button is fully functional

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ PhotocellWaver
 Reads a photocell and turns a servo to keep the lights on.
 """
 
-from machine import ADC, Pin, PWM
+from machine import ADC, Pin
 from picozero import Button
 from servo import Servo
 from time import sleep, sleep_ms
@@ -21,26 +21,44 @@ def Wave(servo_pin: int = 0, led: Pin = Pin(25, Pin.OUT), **kwargs):
             else:
                 delay_ms = kwargs['delay_ms']
 
-    for position in range(0, 180):  # Step the position forward from 0deg to 180deg
+    for position in range(0, 180):
+        """ Step the position forward from 0deg to 180deg. """
         # print(position)  # Show the current position in the Shell/Plotter
         servo.write(position)  # Set the Servo to the current position
-        sleep_ms(delay_ms)  # Wait for the servo to make the movement
+        sleep_ms(delay_ms)  # wait delay_ms before moving to the next number
 
-    for position in reversed(range(0, 180)):  # Step the position reverse from 180deg to 0deg
+    for position in reversed(range(0, 180)):
+        """ Step the position in reverse from 180deg to 0deg. """
         # print(position)  # Show the current position in the Shell/Plotter
         servo.write(position)  # Set the Servo to the current position
-        sleep_ms(delay_ms)  # Wait for the servo to make the movement
+        sleep_ms(delay_ms)  # wait delay_ms before moving to the next number
 
     onboardLED_Blink(led)
 
 
 def onboardLED_Blink(led: Pin = Pin(25, Pin.OUT), sleep_length: float = 0.5):
     led.on()
-    print("LED on")
+    # print("LED on")
     sleep(sleep_length)
     led.off()
-    print("LED off")
+    # print("LED off")
     sleep(sleep_length)
+
+
+def TestButton():
+    test_b = Button(13)
+    while True:
+        print(test_b.is_active)
+
+
+def StartStop(button_pin: int = 13):
+    start_stop_button = Button(button_pin)
+
+    if start_stop_button.is_active:
+        print("start/stop activated.")
+        return start_stop_button
+    else:
+        return False
 
 
 def ReadPhotoCell(**kwargs):
@@ -59,6 +77,7 @@ def ReadPhotoCell(**kwargs):
 
     ldr = ADC(photocell_pin)
     wave_button = Button(manual_button_pin)
+
     while True:
         has_waved = False
         if wave_button.is_active:
@@ -66,15 +85,32 @@ def ReadPhotoCell(**kwargs):
             wave_button.when_activated = Wave
             has_waved = True
 
-        reading = ldr.read_u16()
-        print(f"Photocell reading: {reading}")
-        if reading >= dark_threshold and not has_waved:
+        photo_cell_reading = ldr.read_u16()
+        print(f"Photocell reading: {photo_cell_reading}")
+
+        if photo_cell_reading >= dark_threshold and not has_waved:
             Wave(led=onboard_led_pin)
             has_waved = True
         if has_waved:
             print("has waved")
+
         sleep(1)
+
+        stop = StartStop()
+        if stop:
+            break
+    WaitForStart()
+
+
+def WaitForStart():
+    while True:
+        onboardLED_Blink()
+        go = StartStop()
+        if go:
+            break
+    ReadPhotoCell()
 
 
 if __name__ == '__main__':
-    ReadPhotoCell()
+    print("Waiting for Start.")
+    WaitForStart()
